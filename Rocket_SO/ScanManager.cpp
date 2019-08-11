6f8464bd971d30b2191e1eee8b7a984e719bb55b
Ran1366 <m13669286873_1@163.com>
2019-08-11 16:59:00 +0800

    update

--- Rocket_SO/ScanManager.cpp
+++ Rocket_SO/ScanManager.cpp
@@ -2,8 +2,8 @@
 #include "ScanManager.h"
 ScanManager* ScanManager::_inst = nullptr;
 std::mutex ScanManager::_mtx;
-#define MAXCHANGE 50
-void ScanManager::innerScan(const std::string& path)
+#define MAXCHANGE 1000
+void ScanManager::Scan(const std::string& path)
 {
 	// 比对  文件系统和数据库
 	std::vector<std::string> localdirs;
@@ -19,27 +19,27 @@ void ScanManager::innerScan(const std::string& path)
 
 	auto localit = localset.begin();
 	auto dbit = dbset.begin();
-	static int count = 0;
+	//static int count = 0;
 	while (localit != localset.end() && dbit != dbset.end())
 	{
 		if (*localit < *dbit) // 本地有，数据没有->新增数据
 		{
-			int ret = DataManager::GetInstance()->InsertDoc(path, *localit);
-			if (ret != SQLITE_OK)
+			DataManager::GetInstance()->InsertDoc(path, *localit);
+			/*if (ret != SQLITE_OK)
 			{
 				DataManager::GetInstance()->RollbackTransaction();
 			}
-			++count;
+			++count;*/
 			++localit;
 		}
 		else if (*localit > *dbit) // 本地没有，数据有->删除数据
 		{
-			int ret = DataManager::GetInstance()->DeleteDoc(path, *dbit);
-			if (ret != SQLITE_OK)
+			DataManager::GetInstance()->DeleteDoc(path, *dbit);
+			/*if (ret != SQLITE_OK)
 			{
 				DataManager::GetInstance()->RollbackTransaction();
 			}
-			++count;
+			++count;*/
 			++dbit;
 		}
 		else // 不变的数据
@@ -52,31 +52,31 @@ void ScanManager::innerScan(const std::string& path)
 	while (localit != localset.end())
 	{
 		// 新增数据
-		int ret = DataManager::GetInstance()->InsertDoc(path, *localit);
-		if (ret != SQLITE_OK)
+		DataManager::GetInstance()->InsertDoc(path, *localit);
+		/*if (ret != SQLITE_OK)
 		{
 			DataManager::GetInstance()->RollbackTransaction();
 		}
-		++count;
+		++count;*/
 		++localit;
 	}
 
 	while (dbit != dbset.end())
 	{
 		// 删除数据
-		int ret = DataManager::GetInstance()->DeleteDoc(path, *dbit);
-		if (ret != SQLITE_OK)
+		DataManager::GetInstance()->DeleteDoc(path, *dbit);
+		/*if (ret != SQLITE_OK)
 		{
 			DataManager::GetInstance()->RollbackTransaction();
 		}
-		++count;
+		++count;*/
 		++dbit;
 	}
-	if (count >= MAXCHANGE)
+	/*if (count >= MAXCHANGE)
 	{
 		DataManager::GetInstance()->CommitTransaction();
 		count = 0;
-	}
+	}*/
 
 	// 递归扫描对比子目录数据(to do)
 	for (const auto& subdirs : localdirs)
@@ -85,17 +85,46 @@ void ScanManager::innerScan(const std::string& path)
 		subpath += '\\';
 		subpath += subdirs;
 
-		innerScan(subpath);
+		Scan(subpath);
 	}
 }
 
-void ScanManager::Scan(const std::string& path)
+void ScanManager::toThd()
 {
-	//to do ： 解决在Scan内部只调用一次开启事务和提交事务的问题
-	//在内部统计改动条目
-	//实现改动出错时回滚事务
-	//考虑使用辅助的栈
-	//DataManager::GetInstance()->BeginTransaction();
-	innerScan(path);
-	DataManager::GetInstance()->CommitTransaction();
+	while (1)
+	{
+		std::vector<std::string> localdirs;
+		std::vector<std::string> localfiles;
+		std::string path = "F:";
+		DirectoryList(path, localdirs, localfiles);
+		int len = localdirs.size();
+		std::vector<std::thread> thd_list;
+		thd_list.resize(len);
+		for (auto& subdirs : localdirs)
+		{
+			std::string subpath = path;
+			subpath += '\\';
+			subpath += subdirs;
+			subdirs = subpath;
+		}
+		//DataManager::GetInstance()->BeginTransaction();
+		for (int i = 0; i < len; ++i)
+		{
+			std::thread thd(&ScanManager::Scan, this, localdirs[i]);
+			thd_list[i] = std::move(thd);
+		}
+		//cout << "扫描中，请耐心等待..." << endl;
+		for (int i = 0; i < len; ++i)
+		{
+			thd_list[i].join();
+		}
+		//如果扫描完成后有部分数据未同步
+		DataManager::GetInstance()->Aftermath();
+		Sleep(5000);
+	}
+}
+void ScanManager::StartScan()
+{
+	std::thread scan_thd(&ScanManager::toThd, this);
+	scan_thd.detach();
 }
