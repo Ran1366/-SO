b80e110a8bf8ea4f19cd70d940111b88ad614b59
Ran1366 <44722834+Ran1366@users.noreply.github.com>
2019-08-11 17:06:58 +0800

    Update ScanManager.h

--- Rocket_SO/ScanManager.h
+++ Rocket_SO/ScanManager.h
@@ -5,45 +5,10 @@
 class ScanManager
 {
 public:
-	void innerScan(const std::string& path);
 	void Scan(const std::string& path);
 
-	void StartScan()
-	{
-		//Ñ­»·É¨ÃèÖ¸¶¨Ä¿Â¼£¬¶¨Ê±Ë¯Ãß
-		
-		/*std::vector<std::string> localdirs;
-		std::vector<std::string> localfiles;
-		DirectoryList("E:", localdirs, localfiles);
-		int len = localdirs.size();
-		std::vector<std::thread> thd;
-		for (int i = 0; i < len; ++i)
-		{
-			thd.push_back(std::thread());
-		}
-		for (auto& subdirs : localdirs)
-		{
-			std::string subpath = "E:";
-			subpath += '\\';
-			subpath += subdirs;
-			subdirs = subpath;
-		}*/
-		
-		//Scan("E:");
-		/*for (int i = 0; i < len; ++i)
-		{
-			thd[i] = std::thread(&ScanManager::Scan,CreatInstance(),localdirs[i]);
-			thd[i].detach();
-		}*/
-		//std::thread th(&ScanManager::Scan,this, "E:");
-		//th.join();
-		while (1)
-		{
-			Scan("E:");
-			Sleep(3000);
-		}
-		//¿ÉÄÜ½â¾öÍË³ö³ÌÐò½áÊøÑ­»·µÄÎÊÌâ
-	}
+	void StartScan();
+	void toThd();
 
 	static ScanManager* CreatInstance()
 	{
@@ -63,8 +28,8 @@ private:
 	{
 		//_datamgr.Init();
 	}
-	static ScanManager *_inst;//µ¥Àý¶ÔÏóÖ¸Õë
+	static ScanManager *_inst;//å•ä¾‹å¯¹è±¡æŒ‡é’ˆ
 	static std::mutex _mtx;
-	//Ö»ÐèÒ»¸öÉ¨ÃèÄ£¿é
+	//åªéœ€ä¸€ä¸ªæ‰«ææ¨¡å—
 	//DataManager _datamgr;
 };
